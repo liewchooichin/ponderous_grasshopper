47c26f4ce2bebd81058fe31922be002f9d5205d5
liewchooichin <liewchooichin@gmail.com>
2024-07-05 18:04:43 +0800

    model relationship

--- SQLQuery_bands.sql
+++ SQLQuery_bands.sql
@@ -1,1 +1,27 @@
-select * from dbo.bands_musician;
+--select * from dbo.bands_band;
+-- Drop a table called 'TableName' in schema 'dbo'
+-- Drop the table if it already exists
+--IF OBJECT_ID('[dbo].[TableName]', 'U') IS NOT NULL
+--DROP TABLE [dbo].[TableName]
+--GO
+
+--IF OBJECT_ID('dbo.bands_band', 'U') IS NOT NULL
+--DROP TABLE dbo.bands_band
+--GO
+
+-- Create a new table called '[TableName]' in schema '[dbo]'
+-- Drop the table if it already exists
+--IF OBJECT_ID('[dbo].[TableName]', 'U') IS NOT NULL
+--DROP TABLE [dbo].[TableName]
+--GO
+-- Create the table in the specified schema
+-- CREATE TABLE [dbo].[TableName]
+-- (
+--     [Id] INT NOT NULL PRIMARY KEY, -- Primary Key column
+--     [ColumnName2] NVARCHAR(50) NOT NULL,
+--     [ColumnName3] NVARCHAR(50) NOT NULL
+--     -- Specify more columns here
+-- );
+-- GO
+
+
--- hello/bands/models.py
+++ hello/bands/models.py
@@ -22,3 +22,42 @@ class Musician(models.Model):
     )
     def __str__(self):
         return f"Musician(id={self.id}, {self.first_name} {self.last_name})"
+
+# Venue and Room models and their one-to-many relationship
+
+class Venue(models.Model):
+    """One venue can have many rooms, but a room can only belong to one venue."""
+    """A venue table can list a unique venue."""
+    """The unique Venue id will be a foreign key in the Room table."""
+    """The Room table has a unique room. Each row of unique room"""
+    """will be associated with a Venue through the Venue foreign key."""
+    """If a venue gets removed from the database, it doesn’t"""
+    """make sense to keep the associated rooms, so the on_delete value for""" 
+    """the Room is CASCADE."""
+    id = models.SmallAutoField(primary_key=True)
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f"Venue (id={self.id}, name={self.name})"
+    
+class Room(models.Model):
+    id = models.SmallAutoField(primary_key=True)
+    name = models.CharField(max_length=20)
+    venue = models.ForeignKey("Venue", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Room(id={self.id}, name={self.name})"
+
+# Many-to-many relationship between bands and musicians
+# A musician can belong to many groups.
+# A group can have many musician.
+# It doesn’t matter which model has the ManyToManyField, but you should 
+# only put it in one of the models – not both.
+class BandGroup(models.Model):
+    """A group can have many musicians. Many-to-many relationship."""
+    id = models.SmallAutoField(primary_key=True)
+    name = models.CharField(max_length=20, default="Band Group")
+    members = models.ManyToManyField(to="bands.Musician", verbose_name="Members")
+    
+    def __str__(self):
+        return f"BandGroup(id={self.id}, name={self.name})"
